808f4f11bf08958ed6aeb1c4f88ddc1940a806b0
Ersin Guvenc <eguvenc@gmail.com>
2012-01-23 16:59:11 +0200

    renamed tasks/start.php as hello.php to prevent collisions.

--- tasks/hello.php
+++ tasks/hello.php
@@ -1,7 +1,7 @@
 <?php
 defined('CMD') or exit('Access Denied!');
 
-Class Start extends Controller {
+Class Hello extends Controller {
     
     function __construct()
     {   
@@ -19,7 +19,7 @@ Class Start extends Controller {
   /_______/   /__________/ /________/ /_______/ /_______ /  /_______/ 
   
                 Welcome to Obullo Task Manager (c) 2011.
-     Please run this command [$php task.php start help] for help ! 
+     Please run this command [$php task.php hello help] for help ! 
                 YOU ARE IN /APPLICATION/TASKS FOLDER '."\n\n";
      
     }
@@ -32,14 +32,14 @@ Class Start extends Controller {
     MANAGING TASKS IN /MODULES/TASKS FOLDER
     1 . Obullo has a '/tasks' folder in /modules directory.
     2 . In tasks folder you can create /model, /helpers, /views folders like other modules.
-    3 . Also you can call hmvc requests from other modules.
-    4 . Finally manually you can run a task controller like this : \n\t > \$php task.php controller method argument1 argument2 ...\n
+    3 . Also you can call hmvc requests to other modules.
+    4 . Manually you can run a task controller like this : \n\t > \$php task.php controller method argument1 argument2 ...\n
     
     MANAGING TASKS IN CURRENT MODULE
     1 . You can also create '/tasks' folder in a module like this /modules/welcome/tasks/.
     2 . If you prefer this way you should just put your task controllers to in it, then Obullo tasks\noperations will work from this folder.
     3 . You don't need to create /model, /helpers, /views folders again, you already had it in current\nmodule.
-    4 . Finally manually you can run a task controller like this : \n\t > \$php task.php module controller method argument1 argument2 ...\n
+    4 . Manually you can run a task controller like this : \n\t > \$php task.php module controller method argument1 argument2 ...\n
     look at / User Guide / General Topics / Tasks / for more details.\n\n";
     }
     
