3eacb5516980ab26764ad017c5ef2b1ee89c91c7
Ersin Guvenc <eguvenc@gmail.com>
2012-02-13 02:00:40 +0200

    database.php updated

--- config/database.php
+++ config/database.php
@@ -3,23 +3,16 @@ defined('BASE') or exit('Access Denied!');
 
 /*
 |--------------------------------------------------------------------------
-| Global Active Record Switch
-|--------------------------------------------------------------------------
-*/
-$database['system']['active_record'] = FALSE;
-
-/*
-|--------------------------------------------------------------------------
 | Database Settings
 |--------------------------------------------------------------------------
 | Put your static database configurations here and decide your db variable
-| name. You will use it in the application like this $this->db .
+| name. You will use it in the application with db variable e.g. $this->db .
 | 
-| $database['db']['option']  db variable name ( default $this->db )
+| $database['db']['option']  db variable name ( default is "db" )
 | 
-| Prototype Options:  array( PDO::ATTR_PERSISTENT => FALSE , 
-|                            PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => FALSE
-|                          ); 
+| Prototype: 
+|
+|   $database['db']['options'] = array( PDO::ATTR_PERSISTENT => FALSE); 
 | 
 */
 $database['db']['hostname']  = "localhost";
@@ -27,6 +20,8 @@ $database['db']['username']  = "root";
 $database['db']['password']  = "";
 $database['db']['database']  = "obullo";
 $database['db']['dbdriver']  = "mysql";
+$database['db']['dbprefix']  = '';
+$database['db']['swap_pre']  = '';
 $database['db']['dbh_port']  = "";
 $database['db']['char_set']  = "utf8";
 $database['db']['dsn']       = "";
@@ -38,6 +33,8 @@ $database['db2']['username'] = "root";
 $database['db2']['password'] = "";
 $database['db2']['database'] = "";
 $database['db2']['dbdriver'] = "mysql";
+$database['db2']['dbprefix']  = '';
+$database['db2']['swap_pre']  = '';
 $database['db2']['dbh_port'] = "";
 $database['db2']['char_set'] = "utf8";
 $database['db2']['dsn']      = "";
